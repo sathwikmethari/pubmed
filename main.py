3ee4644c4b98b1c42017dbbc9001c7b8b4011d60
sathwikmethari <sathwikmethari@gmail.com>
2025-07-08 17:30:35 +0530

    added files

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import *
+
+
+url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+
+email, api_key = get_email_and_api(url)
---
+++ src/myscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hello from the shell script!"
+python3 example.py
+read -p "Press Enter to exit..."
